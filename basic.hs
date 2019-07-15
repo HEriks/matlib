8b9a93b89ec94be22764e4611a6068e16bd5c978
Hannes Erikson <eriksonhannes@gmail.com>
2019-07-15 16:56:49 +0200

    prime factorization

--- basic.hs
+++ basic.hs
@@ -1,6 +1,8 @@
 module Basic where
+    {-# LANGUAGE FlexibleContexts #-}
 import Test.QuickCheck
--- Double is used as an approximation of the real numbers
+---- Double is used as an approximation of the real numbers ----
+
 -- Basic functions
 neg ::  Double -> Double
 neg a = minus 0 a
@@ -31,15 +33,41 @@ pow :: Double -> Double -> Double   -- a^b
 pow _ 0   = 1
 pow 0 exp = 0
 pow n exp
-    | exp > 0   = n * (pow n (abs exp - 1)) -- Had to use "*" since "times" does not accept Doubles
+    | exp > 0   = times n (pow n (abs exp - 1))
     | otherwise = 1 / pow n (abs exp) --TODO
 
 -- From the cheat sheet for ma2c
 kvadr1 :: Double -> Double -> Double
-kvadr1 a b = pow (plus a b) 2 
+kvadr1 a b = plus (times a a) (plus (times 2 (times a b)) (times b b)) --pow (plus a b) 2 
 
 kvadr2 :: Double -> Double -> Double
 kvadr2 a b = pow (minus a b) 2 
+
+--- Primes ---
+-- Prime factorization
+primeFactors :: Int -> [Int]
+primeFactors n = pf n []
+
+pf :: Int -> [Int] -> [Int]
+pf n ns | n < 2          = if length ns == 0 then [n] else ns
+        | n `mod` 2 == 0 = pf (div n 2) (ns ++ [2])
+        | otherwise      = pf (div n x) (ns ++ [x])
+            where
+                x = findNextDivisor n 3
+
+findNextDivisor :: Int -> Int -> Int
+findNextDivisor n i | n `mod` i == 0 = i
+                    | n <= i         = n
+                    | otherwise      = findNextDivisor n (i+1)
+
+-- Is a given number a prime?
+isPrime :: Int -> Bool
+isPrime n | n < 2 = False
+          | n < 4 = True
+          | otherwise = ip n 4
+                where
+                    ip n x | x > (floor . sqrt . fromIntegral n) = False
+                           | otherwise = if mod n x == 0 then True else ip n (x+1)
 
 -- QuickCheck to ensure the functions work
 checkMinus :: Double -> Double -> Bool
